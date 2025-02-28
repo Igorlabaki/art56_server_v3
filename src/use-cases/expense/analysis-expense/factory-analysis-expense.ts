c32bbfee095133eadc719a51cc5e471852c8f894
Igor Augusto Labaki Goncalo <igorlabakig@gmail.com>
2025-02-28 18:51:49 +0000

    s

--- src/use-cases/expense/analysis-expense/factory-analysis-expense.ts
+++ src/use-cases/expense/analysis-expense/factory-analysis-expense.ts
@@ -1,7 +1,7 @@
 
 import prismaClient from "../../../service/prisma-client";
 import { AnalysisExpenseUseCase } from "./use-case-analysis-expense";
-import { AnalysisExpenseController } from "./controller-Analysis-expense";
+import { AnalysisExpenseController } from "./controller-analysis-expense";
 import { PrismaExpenseRepository } from "../../../repositories/in-prisma/expense-in-prisma-repository";
 
 export const analysisExpenseFactory = () => {
