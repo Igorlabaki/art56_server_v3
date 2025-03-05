537caf3a600871dd44cdf6bbd360a19f2da0816c
Igor Augusto Labaki Goncalo <igorlabakig@gmail.com>
2025-03-05 15:54:33 +0000

    a

--- src/use-cases/image/create-image/use-case-create-image.ts
+++ src/use-cases/image/create-image/use-case-create-image.ts
@@ -1,11 +1,10 @@
-import { HttpConflictError } from "../../../errors/errors-type/htttp-conflict-error";
-import { CreateImageRequestParams } from "../../../zod/image/create-image-params-schema";
+import { CreateImageDbParams } from "../../../zod/image/create-image-params-db-schema";
 import { ImageRepositoryInterface } from "../../../repositories/interface/image-repository-interface";
 
 class CreateImageUseCase {
   constructor(private imageRepository: ImageRepositoryInterface) {}
 
-  async execute(params : CreateImageRequestParams) {
+  async execute(params : CreateImageDbParams) {
     const newImage = await this.imageRepository.create(params);
 
     const formatedResponse = {
