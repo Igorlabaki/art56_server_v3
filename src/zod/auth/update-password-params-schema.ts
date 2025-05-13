a4f10601c7e49781ea07bf10b88b9a56d080db4f
Igor Goncalo <igorlabakig@gmail.com>
2025-05-13 12:23:55 +0100

    s

--- src/zod/auth/update-password-params-schema.ts
+++ src/zod/auth/update-password-params-schema.ts
@@ -2,9 +2,9 @@ import { z } from "zod";
 
 export const updatePasswordSchema = z.object({
     userId: z.string(),
-    currentPassword: z.string().min(6, "A senha atual deve ter no mínimo 6 caracteres"),
-    newPassword: z.string().min(6, "A nova senha deve ter no mínimo 6 caracteres"),
-    confirmPassword: z.string().min(6, "A confirmação de senha deve ter no mínimo 6 caracteres")
+    currentPassword: z.string().min(4, "A senha atual deve ter no mínimo 6 caracteres"),
+    newPassword: z.string().min(4, "A nova senha deve ter no mínimo 6 caracteres"),
+    confirmPassword: z.string().min(4, "A confirmação de senha deve ter no mínimo 6 caracteres")
 }).refine((data) => data.newPassword === data.confirmPassword, {
     message: "As senhas não conferem",
     path: ["confirmPassword"]
