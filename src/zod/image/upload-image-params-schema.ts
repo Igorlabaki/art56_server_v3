d5ed06c77f29a46243c4eb8c67e462375044e7a6
Igor Goncalo <igorlabakig@gmail.com>
2025-06-04 18:43:17 +0100

    s

--- src/zod/image/upload-image-params-schema.ts
+++ src/zod/image/upload-image-params-schema.ts
@@ -3,9 +3,9 @@ import { z } from "zod";
 export const updateImageRequestParams = z.object({
   imageId: z.string(),
   venueId: z.string(),
-  imageUrl: z.string(),
   description: z.string(),
   tag: z.string().nullable(), // Opcional inicialmente
+  imageUrl: z.string().optional(),
   position: z.string().optional(),  // Opcional inicialmente
   responsiveMode: z.string().optional(), // Opcional inicialmente
 })
