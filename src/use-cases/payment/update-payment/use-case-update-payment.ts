2c27fa67526e118722117d37c3877ee213ae586f
Igor Goncalo <igorlabakig@gmail.com>
2025-06-05 20:46:15 +0100

    s

--- src/use-cases/payment/update-payment/use-case-update-payment.ts
+++ src/use-cases/payment/update-payment/use-case-update-payment.ts
@@ -47,6 +47,7 @@ class UpdatePaymentUseCase {
             data: {
                 amount: Number(amount),
                 paymentDate: formattedDate,
+                imageUrl: imageUrl,
             },
         });
 
