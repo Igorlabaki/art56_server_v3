aaa756ff4ec063891ecf64a7d7df69858f05364d
Igor Goncalo <igorlabakig@gmail.com>
2025-06-11 17:58:34 +0100

    s

--- src/repositories/interface/venue-repository-interface.ts
+++ src/repositories/interface/venue-repository-interface.ts
@@ -1,11 +1,12 @@
 import { Venue, User } from "@prisma/client";
-import { UpdateVenueSchema } from "../../zod/venue/update-venue-params-schema";
+
 import { CreateVenueRequestParams } from "../../zod/venue/create-venue-params-schema";
 import { GetVenueByIdRequestParamSchema } from "../../zod/venue/get-by-id-venue-param-schema";
 import { ListVenueRequestQuerySchema } from "../../zod/venue/list-venue-query-schema";
 import { ListPermittedVenueRequestQuerySchema } from "../../zod/venue/list-venue-permitted-query-schema";
 import { GetSelectedVenueRequestParamSchema } from "../../zod/venue/get-selected-venue-param-schema";
 import { GetVenueAnalyticsParams } from "../../zod/venue/get-venue-analytics-params-schema";
+import { UpdateVenueSchemaDb } from "../../zod/venue/update-venue-params-schema";
 
 export type ItemListVenueResponse = {
   id: string;
@@ -67,7 +68,7 @@ export interface VenueAnalyticsResponse {
 
 export interface VenueRepositoryInterface {
   delete: (params: string) => Promise<Venue | null>
-  update: (params: UpdateVenueSchema) => Promise<Venue | null>
+  update: (params: UpdateVenueSchemaDb) => Promise<Venue | null>
   create: (params: CreateVenueRequestParams) => Promise<Venue | null>
   getById: (params: GetVenueByIdRequestParamSchema) => Promise<Venue | null>
   getSelectedVenue: (params: GetSelectedVenueRequestParamSchema) => Promise<Venue | null>
