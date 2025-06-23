022a8ff104bf36c20961da80e4c7ca06d0a4a970
Igor Goncalo <igorlabakig@gmail.com>
2025-06-23 13:54:22 +0100

    s

--- src/use-cases/image-organization/list-image-organization/controller-list-image-organization.ts
+++ src/use-cases/image-organization/list-image-organization/controller-list-image-organization.ts
@@ -10,16 +10,20 @@ class ListImageOrganizationController {
 
   async handle(req: Request, resp: Response) {
     try {
+      console.log(req.query);
       const query: ListImageOrganizationRequestQuerySchema = listImageOrganizationRequestQuerySchema
         .parse(req.query);
+        console.log(query);
       // Esperar a execução do caso de uso
       const response = await this.listImagesOrganizationUseCase.execute(query);
       // Retornar o token
       return resp.status(200).json(response);
     } catch (error) {
+      console.log(error);
       // Chamar o handleErrors para formatar o erro
       const errorResponse = handleErrors(error);
       // Retornar a resposta formatada
+      console.log(errorResponse);
       return resp.status(errorResponse.statusCode).json(errorResponse.body);
     }
   }
