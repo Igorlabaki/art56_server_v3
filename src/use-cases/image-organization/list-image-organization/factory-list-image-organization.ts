72fc20ecaea55545fee6b671579e828d1feaf5ae
Igor Goncalo <igorlabakig@gmail.com>
2025-06-23 14:05:16 +0100

    s

--- src/use-cases/image-organization/list-image-organization/factory-list-image-organization.ts
+++ src/use-cases/image-organization/list-image-organization/factory-list-image-organization.ts
@@ -1,7 +1,8 @@
-import prismaClient from "../../../services/prisma-client";
-import { ListImagesOrganizationUseCase } from "./use-case-list-image-organization";
-import { ListImageOrganizationController } from "./controller-list-image-organization";
 import { PrismaImageRepository } from "../../../repositories/in-prisma/image-in-prisma-repository";
+import prismaClient from "../../../services/prisma-client";
+import { ListImageOrganizationController } from "./controller-list-image-organization";
+import { ListImagesOrganizationUseCase } from "./use-case-list-image-organization";
+
 
 export const listImageOrganizationFactory = (): ListImageOrganizationController => {
     const prismaImageRepository = new PrismaImageRepository(prismaClient);
