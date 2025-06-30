847b5515f3fd281e89a8a679f9f037faffe41129
Igor Goncalo <igorlabakig@gmail.com>
2025-06-30 16:11:42 +0100

    feat: add organization update images

--- src/repositories/in-prisma/image-in-prisma-repository.ts
+++ src/repositories/in-prisma/image-in-prisma-repository.ts
@@ -2,13 +2,13 @@ import { PrismaClient, Image } from "@prisma/client";
 import { ImageRepositoryInterface } from "../interface/image-repository-interface";
 import { CreateImageRequestParams } from "../../zod/image/create-image-params-schema";
 import { ListImageRequestQuerySchema } from "../../zod/image/list-image-query-schema";
-import { UpdateImageRequestParams } from "../../zod/image/upload-image-params-schema";
 import { GetByTagImageRequestQuerySchema } from "../../zod/image/get-by-tag-image-query-schema";
 import { CreateImageDbParams } from "../../zod/image/create-image-params-db-schema";
 import { CreateImageOrganizationDbParams } from "../../zod/image-organization/create-image-organization-params-db-schema";
 import { UpdateImageOrganizationRequestParams } from "../../zod/image-organization/upload-image-organization-params-schema";
 import { ListImageOrganizationRequestQuerySchema } from "../../zod/image-organization/list-image-organization-query-schema";
 import { GetByTagImageOrganizationRequestQuerySchema } from "../../zod/image-organization/get-by-tag-image-organization-query-schema";
+import { UpdateImageRequestParams } from "../../zod/image/upload-image-params-schema copy";
 /* import { ListImageRequestQuerySchema } from "../../zod/image/list-image-query-schema";
 import { UpdateImageRequestParams } from "../../zod/image/update-image-params-schema"; */
 
@@ -101,7 +101,7 @@ export class PrismaImageRepository implements ImageRepositoryInterface {
     });
   }
 
-  async update({ imageId, position, ...rest }: UpdateImageRequestParams): Promise<Image | null> {
+  async update({ imageId, position, ...rest }: UpdateImageRequestParams ): Promise<Image | null> {
     return await this.prisma.image.update({
       where: {
         id: imageId,
